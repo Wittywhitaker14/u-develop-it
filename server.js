40759c17f53bc18dcfbb5a87896437276b3eb582
Austin Whitaker <wittywhitaker14@gmail.com>
2021-12-12 15:08:41 -0700

    made a select queries function

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const { keyword } = require('chalk');
 const express = require('express');
+const mysql = require('mysql2');
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: '127.0.0.1',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'Secondamendment_2',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -7,6 +22,9 @@ const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
 
 app.get('/', (req, res) => {
     res.json({
